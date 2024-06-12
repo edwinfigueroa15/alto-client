b04182ac1eeb83bb8f0ee5c145b4806272095c01
edwinfigueroa15 <edwinfigueroa15@hotmail.com>
2024-06-12 14:59:34 -0500

    Dashboard

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { Menu } from 'app/core/interfaces/components.interface';
 import Modules from 'app/shared/modules';
 
 @Component({
@@ -9,5 +11,28 @@ import Modules from 'app/shared/modules';
   styleUrl: './admin.component.scss',
 })
 export default class AdminComponent {
+  showMenu = signal(false);
+  currentPath: string = '';
+  pages: Menu[] = [
+    { title: 'Estadisticas', url: '/admin/statistics', icon: 'assets/icons/' },
+    { title: 'Equipos', url: '/admin/team', icon: 'assets/icons/' },
+    { title: 'Partidos', url: '/admin/matches', icon: 'assets/icons/' },
+  ]
 
+  private _router = inject(Router);
+
+  async ngOnInit() {
+    this.currentPath = this._router.url;
+    this._router.events.subscribe((event: any) => {
+      if (event?.url) this.currentPath = event?.url;
+    })
+  }
+
+  changeShowMenu() {
+    this.showMenu.update(value => !value);
+  }
+
+  closeMenu() {
+    this.showMenu.update(value => false);
+  }
 }
---
+++ src/app/admin/pages/statistics/statistics.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import Modules from 'app/shared/modules';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: true,
+  imports: [Modules],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.scss'
+})
+export default class StatisticsComponent {
+
+}
